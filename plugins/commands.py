bb9755f24bcb3fc4be3170f880c76ad0190daa1a
Umaidahmad01 <ahmedumaid707@gmail.com>
2024-11-08 20:27:57 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,7 +42,7 @@ def get_size(size):
 # Ask Doubt on telegram @KingVJ0
 
 
-@Client.on_message(@app.on_message(filters.command("start"))
+@App.on_message(filters.command("start"))
 @force_sub  # Apply subscription check to the /start command
 async def start(client, message):
     await message.reply("Welcome! You have access to this command.")
